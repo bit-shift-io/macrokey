3c696d1fe22d6428fa7bd2a2f9fdcec14d05d1de
Bronson <bronsonmathews@gmail.com>
2025-01-06 10:23:48 +1030

    input device names

--- rs/src/event_device.rs
+++ rs/src/event_device.rs
@@ -1,10 +1,52 @@
 use log::info;
 use tokio::fs;
+use std::io::{Error, ErrorKind};
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use nix::ioctl_read;
+use nix::fcntl::{open, OFlag};
+use nix::unistd::close;
+use nix::sys::stat::Mode;
+use std::str;
 
-
+// Define the ioctl for EVIOCGNAME as a custom ioctl call
+ioctl_read!(evio_get_name, b'E', 0x06, [c_char; 256]);
 
 pub struct EventDevice {
-    filename: String,
+    name: String,
+    path: String,
+}
+
+
+impl EventDevice {
+    fn new(device: &str) -> Result<Self, Error> {
+        // Open the device file
+        let fd = open(device, OFlag::O_RDONLY, Mode::empty())
+            .map_err(|_| Error::new(ErrorKind::InvalidData, "Failed to open device"))?;
+
+        // Get the device name using ioctl
+        let mut nm = [0 as c_char; 256]; // Prepare the buffer for the device name
+        unsafe {
+            let _ = evio_get_name(fd, &mut nm);
+            // if let Err(_) = evio_get_name(fd, &mut nm) {
+            //     info!("Failed to get device name for: {}", device);
+            // }
+        }
+        let name = unsafe { CStr::from_ptr(nm.as_ptr()) }
+            .to_string_lossy()
+            .into_owned();
+    
+        info!("{} -> {}", device, name);
+    
+        // Close the file descriptor
+        close(fd).unwrap_or(());
+
+        Ok(EventDevice {
+            name,
+            path: device.to_string(),
+        })
+    }
+
 }
 
 async fn is_event_device(entry: &fs::DirEntry) -> bool {
@@ -13,10 +55,6 @@ async fn is_event_device(entry: &fs::DirEntry) -> bool {
     if !file_name.starts_with("by-id") && !file_name.starts_with("by-path") {
         return true;
     }
-    // old cpp code, this excludes mice and other devices not startign with event
-    // if let Some(file_name) = entry.file_name().to_str() {
-    //     return file_name.starts_with("event");
-    // }
     false
 }
 
@@ -36,8 +74,8 @@ pub async fn get_system_devices() -> Vec<EventDevice> {
         if is_event_device(&entry).await {
             let path = entry.path();
             let filename = path.to_string_lossy().into_owned();
-            list.push(EventDevice { filename: filename.clone() });
-            info!("{}", filename);
+            list.push(EventDevice::new(&filename).unwrap());
+            //info!("{}", filename);
         }
     }
 
