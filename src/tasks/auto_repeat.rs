ea732a18af8ba6678118d351918ccf1f1ba81286
Bronson <bronsonmathews@gmail.com>
2025-01-13 22:41:06 +1030

    tidy aut repeat

--- src/tasks/auto_repeat.rs
+++ src/tasks/auto_repeat.rs
@@ -26,14 +26,16 @@ use crate::{
 };
 
 const TASK_ID: &str = "AUTO REPEAT";
+static STATE: Lazy<Mutex<State>> = Lazy::new(|| Mutex::new(State::new()));
+
 
 #[derive(Debug)]
 struct State {
     alt_pressed: bool,
     ctrl_pressed: bool,
     capslock_pressed: bool,
-    //meta_pressed: bool,
-    active_events: HashMap<KeyCode, (JoinHandle<()>, InputEvent)>,
+    meta_pressed: bool,
+    repeat_events: HashMap<KeyCode, (JoinHandle<()>, InputEvent)>,
 }
 
 impl State {
@@ -42,8 +44,26 @@ impl State {
             alt_pressed: false,
             ctrl_pressed: false,
             capslock_pressed: false,
-            //meta_pressed: false,
-            active_events: HashMap::new(),
+            meta_pressed: false,
+            repeat_events: HashMap::new(),
+        }
+    }
+
+    fn update_state(&mut self, ev: &InputEvent) {
+        match ev.destructure() {
+            EventSummary::Key(_, KeyCode::KEY_LEFTALT | KeyCode::KEY_RIGHTALT, value) => {
+                self.alt_pressed = value == KeyEventType::PRESSED || value == KeyEventType::REPEAT;
+            }
+            EventSummary::Key(_, KeyCode::KEY_LEFTCTRL | KeyCode::KEY_RIGHTCTRL, value) => {
+                self.ctrl_pressed = value == KeyEventType::PRESSED || value == KeyEventType::REPEAT;
+            }
+            EventSummary::Key(_, KeyCode::KEY_LEFTMETA | KeyCode::KEY_RIGHTMETA, value) => {
+                self.meta_pressed = value == KeyEventType::PRESSED || value == KeyEventType::REPEAT;
+            }
+            EventSummary::Led(_, LedCode::LED_CAPSL, value) => {
+                self.capslock_pressed = value == 1;
+            }
+            _ => {}
         }
     }
 
@@ -85,19 +105,21 @@ impl State {
         else { return false };
 
         // modifier keys here
+        // and special keys to ignore
         match ev.destructure() {
             EventSummary::Key(_, KeyCode::KEY_LEFTALT | KeyCode::KEY_RIGHTALT, _) => { return false } // alt key
             EventSummary::Key(_, KeyCode::KEY_LEFTCTRL | KeyCode::KEY_RIGHTCTRL, _) => { return false } // ctl key
-            //EventSummary::Key(_, KeyCode::KEY_LEFTMETA | KeyCode::KEY_RIGHTMETA, _) => { return false} // meta
+            EventSummary::Key(_, KeyCode::KEY_LEFTMETA | KeyCode::KEY_RIGHTMETA, _) => { return false } // meta
+            EventSummary::Key(_, KeyCode::KEY_CAPSLOCK, _) => { return false } // caps
             _ => {}
         }
 
         true
     }
 
-    fn is_active_event(&mut self, ev: InputEvent) -> bool {
+    fn is_active_repeat_event(&mut self, ev: InputEvent) -> bool {
         let key = KeyCode::new(ev.code());
-        self.active_events.contains_key(&key)
+        self.repeat_events.contains_key(&key)
     }
 
     fn is_stop_all_key(&mut self, ev: InputEvent) -> bool {
@@ -108,8 +130,6 @@ impl State {
     fn is_toggle_pause(&mut self, ev: InputEvent) -> bool {
         // monitoring the led state, not the button itself
         if ev.event_type() == EventType::LED && ev.code() == LedCode::LED_CAPSL.0 { return true }
-        //let key = KeyCode::new(ev.code());
-        //key == KeyCode::KEY_CAPSLOCK
         false
     }
 
@@ -117,42 +137,41 @@ impl State {
         self.capslock_pressed
     }
 
-    fn stop_active_event(&mut self, key: KeyCode) {
-        if let Some(value) = self.active_events.remove(&key) {
+    fn stop_repeat_event(&mut self, key: KeyCode) {
+        if let Some(value) = self.repeat_events.remove(&key) {
             value.0.abort();
         }
     }
 
-    fn start_active_event(&mut self, key: KeyCode, handle: JoinHandle<()>, ie: InputEvent) {
-        self.active_events.insert(key, (handle, ie));
+    fn start_repeat_event(&mut self, key: KeyCode, ie: InputEvent) {
+        let handle = tokio::spawn(repeat_event(ie));
+        self.repeat_events.insert(key, (handle, ie));
     }
 
-    fn stop_all_active_events(&mut self) {
-        for value in self.active_events.values() {
+    fn stop_all_repeat_events(&mut self) {
+        for value in self.repeat_events.values() {
             value.0.abort();
         }
-        self.active_events.clear();
+        self.repeat_events.clear();
     }
 
-    fn pause_all_active_events(&mut self) {
-        for value in self.active_events.values() {
+    fn pause_all_repeat_events(&mut self) {
+        for value in self.repeat_events.values() {
             value.0.abort();
         }
     }
 
-    fn resume_all_active_events(&mut self) {
+    fn resume_all_repeat_events(&mut self) {
         let mut new_events = Vec::new();
-        for value in self.active_events.values() {
+        for value in self.repeat_events.values() {
             let handle = tokio::spawn(repeat_event(value.1.clone()));
             new_events.push((KeyCode::new(value.1.code()), (handle, value.1)));
         }
         for (key, value) in new_events {
-            self.active_events.insert(key, value);
+            self.repeat_events.insert(key, value);
         }
     }
 }
-
-static STATE: Lazy<Mutex<State>> = Lazy::new(|| Mutex::new(State::new()));
 
 
 pub async fn task() {
@@ -160,10 +179,6 @@ pub async fn task() {
 
     loop {
         let devices = functions::get_devices_by_regex("keyboard");
-        if devices.len() == 0 {
-            info!("{} No devices found matching: {}", TASK_ID, "keyboard");
-            return;
-        }
 
         let mut set = JoinSet::new();
         for device in devices {
@@ -185,54 +200,35 @@ async fn process_input(ev: InputEvent) -> () {
     //info!(" > {:?}", ev.destructure());
 
     let mut state = STATE.lock().await;
-
-    // process modifiers
-    set_modifier_state(&mut state, &ev);
+    state.update_state(&ev);
 
     // timers start
-    // with key modifier (ctrl + alt) + key
-    if state.all_modifiers_pressed() && state.is_repeatable(ev) && !state.is_active_event(ev) {
-        state.start_active_event(KeyCode::new(ev.code()), tokio::spawn(repeat_event(ev)), ev);
+    // all modifiers pressed + repeatable key + not already a repeat
+    if state.all_modifiers_pressed() && state.is_repeatable(ev) && !state.is_active_repeat_event(ev) {
+        state.start_repeat_event(KeyCode::new(ev.code()), ev);
     }
 
     // timers end
-    // with single key press
-    // ensure no modifier keys are active
+    // no modifiers pressed + repeatable key
     if !state.any_modifier_pressed() && state.is_repeatable(ev) {
-        // same key pressed
-        if state.is_active_event(ev) { // todo: flakey, modifiers stop working when using these... why?
+        // active repeat event
+        if state.is_active_repeat_event(ev) { // todo: flakey, modifiers stop working when using these... why?
             info!("{}: stop!", TASK_ID);
-            state.stop_active_event(KeyCode::new(ev.code()));
+            state.stop_repeat_event(KeyCode::new(ev.code()));
         }
 
         // delete all timers
+        // stop all key
         if state.is_stop_all_key(ev) {
-            state.stop_all_active_events();
+            state.stop_all_repeat_events();
         }
     }
-    // toggle pause/resume
+    // no modifiers pressed + toggle key(led)
     else if !state.any_modifier_pressed() && state.is_toggle_pause(ev) {
-        if state.is_toggle_pressed() {
-            state.pause_all_active_events();
-        } else {
-            state.resume_all_active_events();
+        match state.is_toggle_pressed() {
+            true => state.pause_all_repeat_events(),
+            false => state.resume_all_repeat_events(),
         }
-    }
-}
-
-
-fn set_modifier_state(state: &mut State, ev: &InputEvent) {
-    match ev.destructure() {
-        EventSummary::Key(_, KeyCode::KEY_LEFTALT | KeyCode::KEY_RIGHTALT, value) => {
-            state.alt_pressed = value == KeyEventType::PRESSED || value == KeyEventType::REPEAT;
-        }
-        EventSummary::Key(_, KeyCode::KEY_LEFTCTRL | KeyCode::KEY_RIGHTCTRL, value) => {
-            state.ctrl_pressed = value == KeyEventType::PRESSED || value == KeyEventType::REPEAT;
-        }
-        EventSummary::Led(_, LedCode::LED_CAPSL, value) => {
-            state.capslock_pressed = value == 1;
-        }
-        _ => {}
     }
 }
 
@@ -250,6 +246,7 @@ pub async fn repeat_event(ie: InputEvent) {
     let pressed_time = 100; //ms
     let released_time = 350; //ms
     let tx = signals::get_virtual_device_tx().await;
+    //info!("{}: start repeat: {:?}", TASK_ID, ie.destructure());
     let key_code = ie.code();
     let press = InputEvent::new_now(EventType::KEY.0, key_code, KeyEventType::PRESSED.into());
     let release = InputEvent::new_now(EventType::KEY.0, key_code, KeyEventType::RELEASED.into());
